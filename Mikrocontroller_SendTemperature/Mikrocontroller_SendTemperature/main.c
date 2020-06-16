691bc77b53cb6903570b4c7228be11d15557009d
Simon <s1monp@mailbox.org>
2020-06-16 18:48:08 +0200

    Initial commit Mikrocontroller

---
+++ Mikrocontroller_SendTemperature/Mikrocontroller_SendTemperature/main.c
@@ -0,0 +1,17 @@
+/*
+ * Mikrocontroller_SendTemperature.c
+ *
+ * Created: 16.06.2020 18:46:22
+ * Author : Simon
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    while (1) 
+    {
+    }
+}
+
