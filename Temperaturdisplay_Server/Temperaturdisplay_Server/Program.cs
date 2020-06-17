0e742b43344f0d5a37e723a14537eab0f3cb57e3
Simon <s1monp@mailbox.org>
2020-06-17 19:38:49 +0200

    SerielPort Implementation Finish

--- Temperaturdisplay_Server/Temperaturdisplay_Server/Program.cs
+++ Temperaturdisplay_Server/Temperaturdisplay_Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace Temperaturdisplay_Server
@@ -8,7 +9,24 @@ namespace Temperaturdisplay_Server
     {
         static void Main(string[] args)
         {
-            Console.ReadKey();
+            Console.WriteLine("Temperaturserver by Simon." +
+                "\nBeginne mit dem Einrichtungsvorgang." +
+                "\nGeben Sie zuerst die erforderlichen Informationen für die Verbindung zum Mikrocontroller ein.");
+            // Einlesen der Daten
+            string portName = SerialPortReader.getPortName();
+            int baudRate = SerialPortReader.getBaudRate();
+            Parity parity = SerialPortReader.getParity();
+            int dataBits = SerialPortReader.getDataBits();
+            StopBits stopBits = SerialPortReader.getStopBits();
+            // Konfiguration der seriellen Schnittstelle
+            SerialPortReader.setSerialSettings(portName, baudRate, parity, dataBits, stopBits);
+            Console.WriteLine("Initialisierung der seriellen Schnittstelle abgeschlossen. Beginne Datenübertragung.");
+            Thread.Sleep(3000);
+            Console.Clear();
+            while (true)
+            {
+                Console.WriteLine(SerialPortReader.read()); 
+            }
         }
     }
 }
--- Temperaturdisplay_Server/Temperaturdisplay_Server/SerialPortReader.cs
+++ Temperaturdisplay_Server/Temperaturdisplay_Server/SerialPortReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Text.RegularExpressions;
 
 namespace Temperaturdisplay_Server
 {
@@ -30,6 +31,8 @@ namespace Temperaturdisplay_Server
             // Setze Schreib / Lese Timeouts
             _serialPort.ReadTimeout = 1500;
             _serialPort.WriteTimeout = 1500;
+
+            _serialPort.Open();
         }
 
         /// <summary>
@@ -39,10 +42,123 @@ namespace Temperaturdisplay_Server
         /// <exception cref="TimeoutException"></exception>
         public static string read()
         {
-            _serialPort.Open();
-
             // Liest den übertragenen Wert und gibt diesen zurück
             return _serialPort.ReadLine();
+        }
+
+        public static string getPortName()
+        {
+            string portName;
+            Console.WriteLine("\nVerfügbare Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("\nGeben Sie den gewünschten Portnamen ein:");
+            do
+            {
+                portName = Console.ReadLine();
+                if (!Regex.IsMatch(portName, "^[cC][oO][mM][1-9]$"))
+                {
+                    Console.Write("Bitte gültigen Portnamen eingeben: ");
+                }
+            }
+            while (!Regex.IsMatch(portName, "^[cC][oO][mM][1-9]$"));
+            return portName;
+        }
+
+        public static int getBaudRate()
+        {
+            string baudRateStr;
+            int baudRate;
+            Console.Write("Geben Sie nun die Baudrate ein: ");
+            do
+            {
+                baudRateStr = Console.ReadLine();
+                if (!Int32.TryParse(baudRateStr, out baudRate))
+                {
+                    Console.Write("Bitte nur Zahlen eingeben: ");
+                }
+            }
+            while (!Int32.TryParse(baudRateStr, out baudRate));
+            return baudRate;
+        }
+
+        public static Parity getParity()
+        {
+            string selector;
+            Console.WriteLine("Verfügbare Paritätswerte:" +
+                "\n(1) None" +
+                "\n(2) Odd" +
+                "\n(3) Even" +
+                "\n(4) Mark " +
+                "\n(5) Space");
+
+            Console.Write("Geben Sie eine Nummer ein: ");
+            do
+            {
+                selector = Console.ReadLine();
+                if (!Regex.IsMatch(selector, "^[1-5]$"))
+                {
+                    Console.Write("Bitte nur Nummern von 1 bis 5 eingeben: ");
+                }
+            }
+            while (!Regex.IsMatch(selector, "^[1-5]$"));
+
+            switch (Int32.Parse(selector))
+            {
+                case 1:
+                    return Parity.None;
+                case 2:
+                    return Parity.Odd;
+                case 3:
+                    return Parity.Even;
+                case 4:
+                    return Parity.Mark;
+                case 5:
+                    return Parity.Space;
+            }
+            return Parity.None;
+        }
+
+        public static int getDataBits()
+        {
+            string dataBits;
+            Console.Write("Geben Sie die Anzahl an Databits ein (5-8): ");
+            do
+            {
+                dataBits = Console.ReadLine();
+                if (!Regex.IsMatch(dataBits, "^[5-8]$"))
+                {
+                    Console.Write("Bitte nur Nummern von 5 bis 8 eingeben: ");
+                }
+            }
+            while (!Regex.IsMatch(dataBits, "^[5-8]$"));
+            return Int32.Parse(dataBits);
+        }
+
+        public static StopBits getStopBits()
+        {
+            string stopBits;
+            Console.Write("Geben Sie die Anzahl an Stoppbits ein (1 oder 2): ");
+            do
+            {
+                stopBits = Console.ReadLine();
+                if (!Regex.IsMatch(stopBits, "^[1-2]$"))
+                {
+                    Console.Write("Bitte nur 1 oder 2 eingeben: ");
+                }
+            }
+            while (!Regex.IsMatch(stopBits, "^[1-2]$"));
+            switch (Int32.Parse(stopBits))
+            {
+                case 1:
+                    return StopBits.One;
+                case 2:
+                    return StopBits.Two;
+            }
+            return StopBits.One;
         }
     }
 }
