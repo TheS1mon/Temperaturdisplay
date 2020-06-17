2c13f0edc57230997a8c5670f53e3ab7c6137f33
Simon <s1monp@mailbox.org>
2020-06-17 22:19:08 +0200

    SQL Anbindung fertiggestellt

--- Temperaturdisplay_Server/Temperaturdisplay_Server/Program.cs
+++ Temperaturdisplay_Server/Temperaturdisplay_Server/Program.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.IO.Ports;
-using System.Text.RegularExpressions;
 using System.Threading;
 
 namespace Temperaturdisplay_Server
@@ -9,9 +9,12 @@ namespace Temperaturdisplay_Server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Temperaturserver by Simon." +
-                "\nBeginne mit dem Einrichtungsvorgang." +
-                "\nGeben Sie zuerst die erforderlichen Informationen für die Verbindung zum Mikrocontroller ein.");
+            Console.WriteLine("Temperaturserver by Simon.\nBeginne mit dem Einrichtungsvorgang.");
+            Thread.Sleep(3000);
+            Console.Clear();
+
+            //Serielle Schnittstelle
+            Console.WriteLine("Geben Sie zuerst die erforderlichen Informationen für die Verbindung via serieller Schnittstelle ein.");
             // Einlesen der Daten
             string portName = SerialPortReader.getPortName();
             int baudRate = SerialPortReader.getBaudRate();
@@ -20,12 +23,69 @@ namespace Temperaturdisplay_Server
             StopBits stopBits = SerialPortReader.getStopBits();
             // Konfiguration der seriellen Schnittstelle
             SerialPortReader.setSerialSettings(portName, baudRate, parity, dataBits, stopBits);
-            Console.WriteLine("Initialisierung der seriellen Schnittstelle abgeschlossen. Beginne Datenübertragung.");
+            Console.WriteLine("Initialisierung der seriellen Schnittstelle abgeschlossen. Weiter gehts mit der Verbindung zum SQL-Server.");
+            Thread.Sleep(3000);
+
+            // MySQL-Anbindung
+            while (!dbSetup()) { } // Stellt die Verbindung zum MySql Server her.
+            Console.WriteLine("Erfolgreich! Ab jetzt werden die Temperaturwerte in der Datenbank gespeichert.");
             Thread.Sleep(3000);
             Console.Clear();
+
+            // Empfangen der Temperaturen und Speicherung in der DB (in eigenem Thread)
+            Thread readAndSave = new Thread(new ThreadStart(readTempAndSaveToDB));
+            readAndSave.Start();
+        }
+
+        private static bool dbSetup()
+        {
+            Console.Clear();
+            Console.WriteLine("Verbindungsassistent zum Verbinden mit einem MySql Server.");
+            SQLConnector.makeConnectionString();
+            Console.WriteLine("Verbindung wird hergestellt..");
+            Thread.Sleep(2000);
+            try
+            {
+                SQLConnector.connectToDB();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Verbindung fehlgeschlagen: " + ex.Message + "\nWiederhole Prozedur, einen Moment bitte..");
+                Thread.Sleep(5000);
+                return false;
+            }
+            Console.WriteLine("Verbindung erfolgreich hergestellt!" + "\nDatenbank wird erstellt..");
+            try
+            {
+                SQLConnector.setupDB();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erstellung fehlgeschlagen: " + ex.Message + "\nWiederhole Prozedur, einen Moment bitte..");
+                Thread.Sleep(5000);
+                return false;
+            }
+            Console.WriteLine("Datenbank erfolgreich erstellt!");
+            return true;
+        }
+
+        private static void readTempAndSaveToDB()
+        {
+            //Empfangen und speichern in DB
+            int counter = 0;
             while (true)
             {
-                Console.WriteLine(SerialPortReader.read()); 
+                if (counter == 9) // Nur jede 10. Messung in DB speichern aufgrund der Auslastung
+                {
+                    SQLConnector.insertToDB(SerialPortReader.read());
+                    counter = 0;
+                    Console.WriteLine("(" + DateTime.Now.ToString("hh:mm:ss") + ") Neuer Wert empfangen und in Datenbank gespeichert.");
+                }
+                else
+                {
+                    SerialPortReader.read();
+                    counter++;
+                }
             }
         }
     }
---
+++ Temperaturdisplay_Server/Temperaturdisplay_Server/SQLConnector.cs
@@ -0,0 +1,103 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Text.RegularExpressions;
+
+namespace Temperaturdisplay_Server
+{
+    class SQLConnector
+    {
+        private static string _connectionString; // "server=;user=;port=;password=;"
+        private static MySqlConnection _conn;
+
+        /// <summary>
+        /// Fordert den Nutzer auf Daten einzugeben und erzeugt aus diesen den ConnectionString für die Datenbank.
+        /// Form "server=;user=;port=;password=;"
+        /// </summary>
+        public static void makeConnectionString()
+        {
+            string server, uid, pwd, port;
+
+            // Server-IP
+            Console.Write("Geben Sie die Server-IP ein: ");
+            do
+            {
+                server = Console.ReadLine();
+                if (!Regex.IsMatch(server, @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"))
+                {
+                    Console.Write("Bitte eine gültige IP eingeben: ");
+                }
+            }
+            while (!Regex.IsMatch(server, @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"));
+
+            // Port
+            Console.Write("Geben Sie den Port ein: ");
+            do
+            {
+                port = Console.ReadLine();
+                if (!Regex.IsMatch(port, @"^\d+$"))
+                {
+                    Console.Write("Bitte einen gültigen Port eingeben: ");
+                }
+            }
+            while (!Regex.IsMatch(port, @"^\d+$"));
+
+            // Benutzernamen
+            Console.Write("Geben Sie nun Ihren Benutzernamen ein: ");
+            uid = Console.ReadLine();
+
+            // Passwort
+            Console.Write("Geben Sie das Passwort von {0} ein: ", uid);
+            pwd = Console.ReadLine();
+
+            _connectionString = "server=" + server + ";user=" + uid + ";port=" + port + ";password=" + pwd + ";";
+        }
+
+        /// <summary>
+        /// Erstellt die Datenbank temperaturlogger und die Tabelle temperatur (temp_ID, temperatur, datum, zeit)
+        /// </summary>
+        /// <exception cref="Exception"></exception>
+        public static void setupDB()
+        {
+            Console.Write("Sollen eventuell bereits vorhandene Datenbanken gelöscht werden? (y / N) ");
+            if(Console.ReadLine().ToLower().Equals("y"))
+            {
+                runNonQuery("drop database `temperaturlogger`");
+            }
+            runNonQuery("CREATE DATABASE IF NOT EXISTS `temperaturlogger`");
+            runNonQuery("use `temperaturlogger`");
+            runNonQuery("CREATE TABLE IF NOT EXISTS `temperatur`" +
+                "(" +
+                "temp_ID INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL, " +
+                "temperatur float NOT NULL, " +
+                "datum date NOT NULL," +
+                "zeit time NOT NULL);");
+
+        }
+
+        /// <summary>
+        /// Fügt die übergebene Temperatur und das aktuelle Datum sowie die Uhrzeit der Datenbank hinzu
+        /// </summary>
+        /// <param name="temp"></param>
+        /// <exception cref="Exception"></exception>
+        public static void insertToDB(string temp)
+        {
+            runNonQuery("INSERT INTO `temperatur` (temperatur, datum, zeit) VALUES (" + temp + ", CURDATE(), CURTIME());");
+        }
+
+        /// <summary>
+        /// Stellt eine Verbindung mit der Datenbank her
+        /// </summary>
+        /// <exception cref="MySqlException"></exception>
+        public static void connectToDB()
+        {
+            _conn = new MySqlConnection(_connectionString);
+            _conn.Open();
+        }
+
+        private static void runNonQuery(string query)
+        {
+            MySqlCommand cmd = new MySqlCommand(query, _conn);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
